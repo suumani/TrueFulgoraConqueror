70b1216c299ef6caa70371ef97c54506e09ca3a7
d.watanabe <daisuke.watanabe@citron-reiwa.com>
2025-01-27 22:06:05 +0900

    ver.0.0.2 Added 'Locator' Items.

---
+++ defines/constant_entity_name.lua
@@ -0,0 +1,9 @@
+-- ----------------------------
+-- ENTITY NAMES
+-- ----------------------------
+CONST_ENTITY_NAME = {
+    SMALL_WORM = "small-worm-turret",
+    MEDIUM_WORM = "medium-worm-turret",
+    BIG_WORM = "big-worm-turret",
+    BEHEMOTH_WORM = "behemoth-worm-turret",
+}
